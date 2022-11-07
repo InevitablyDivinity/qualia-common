181cba51fa4089346db368cd3d53f5c6878fa37e
EchoesForeAndAft <71880558+EchoesForeAndAft@users.noreply.github.com>
2022-11-07 20:18:36 +1000

    feat: Implement basic ql::Tuple and functions for manipulating parameter packs

---
+++ include/common/tuple.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#include "common/types.hpp"
+#include <type_traits>
+#include <utility>
+
+namespace ql
+{
+
+template<std::size_t I, typename T>
+class TupleElement
+{
+public:
+
+  T value;
+};
+
+template<typename Firsts, typename Seconds>
+class TupleBase;
+
+template<std::size_t... Firsts, typename... Seconds>
+class TupleBase<std::index_sequence<Firsts...>, parameter_pack<Seconds...>>
+  : public TupleElement<Firsts, Seconds>...
+{
+};
+
+template<typename... Ts>
+class Tuple
+  : public TupleBase<std::index_sequence_for<Ts...>, parameter_pack<Ts...>>
+{
+public:
+
+  static constexpr std::size_t size = sizeof...( Ts );
+
+  template<std::size_t I>
+  constexpr auto& get()
+  {
+    auto value_type    = get_nth_type<I>( parameter_pack<Ts...>() );
+    using element_type = TupleElement<I, typename decltype( value_type )::type>;
+    return element_type::value;
+  }
+
+  template<std::size_t I>
+  constexpr const auto& get() const
+  {
+    auto value_type    = get_nth_type<I>( parameter_pack<Ts...>() );
+    using element_type = TupleElement<I, typename decltype( value_type )::type>;
+    return element_type::value;
+  }
+};
+
+} // namespace ql
+
+template<typename... Ts>
+struct std::tuple_size<ql::Tuple<Ts...>>
+{
+  static constexpr std::size_t value = ql::Tuple<Ts...>::size;
+};
+
+template<std::size_t I, typename... Ts>
+struct std::tuple_element<I, ql::Tuple<Ts...>>
+{
+  using type =
+    typename decltype( ql::get_nth_type<I>( ql::parameter_pack<Ts...>() ) )::type;
+};
+
+template<std::size_t I, typename... Ts>
+constexpr auto& std::get( ql::Tuple<Ts...>& t )
+{
+  return t.template get<I>();
+}
+
+template<std::size_t I, typename... Ts>
+constexpr const auto& std::get( const ql::Tuple<Ts...>& t )
+{
+  return t.template get<I>();
+}
---
+++ include/common/types.hpp
@@ -0,0 +1,86 @@
+#pragma once
+#include "common/functional.hpp"
+#include <cstddef>
+#include <type_traits>
+
+namespace ql
+{
+
+template<typename... Ts>
+struct parameter_pack
+{
+};
+
+template<typename T>
+struct callable_type;
+
+template<typename R, typename... Args>
+struct callable_type<R( Args... )>
+{
+  using return_type    = R;
+  using argument_types = parameter_pack<Args...>;
+};
+
+template<typename R, typename... Args>
+struct callable_type<R ( * )( Args... )> : callable_type<R( Args... )>
+{
+};
+
+template<typename R, typename... Args>
+struct callable_type<R ( & )( Args... )> : callable_type<R( Args... )>
+{
+};
+
+template<typename R, typename... Args>
+struct callable_type<Function<R( Args... )>> : callable_type<R( Args... )>
+{
+};
+
+template<typename T>
+using return_type_t = typename callable_type<T>::return_type;
+
+template<typename T>
+using argument_types_t = typename callable_type<T>::argument_types;
+
+template<typename T, typename... Ts>
+consteval auto get_first_type( parameter_pack<T, Ts...> )
+{
+  return std::type_identity<T>();
+}
+
+template<std::size_t N, std::size_t I, typename T, typename... Ts>
+consteval auto remove_n_types( parameter_pack<T, Ts...> )
+{
+  if constexpr ( N == I )
+  {
+    return parameter_pack<T, Ts...>();
+  }
+  else
+  {
+    static_assert( sizeof...( Ts ) != 0 );
+    return remove_n_types<N, I + 1>( parameter_pack<Ts...>() );
+  }
+}
+
+template<std::size_t N, typename T, typename... Ts>
+consteval auto remove_n_types( parameter_pack<T, Ts...> )
+{
+  // Don't remove anything.
+  if constexpr ( N == 0 )
+  {
+    return parameter_pack<T, Ts...>();
+  }
+  else
+  {
+    static_assert( sizeof...( Ts ) > 0 );
+    return remove_n_types<N, 1>( parameter_pack<Ts...>() );
+  }
+}
+
+template<std::size_t I, typename... Ts>
+consteval auto get_nth_type( parameter_pack<Ts...> p )
+{
+  return get_first_type( remove_n_types<I>( p ) );
+}
+
+} // namespace ql
